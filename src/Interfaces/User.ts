d7b0db935a2be61c96e39112578fe15306742b9e
Tolfix <matteus12122@gmail.com>
2021-07-21 14:14:50 +0200

    Updated

--- src/Interfaces/User.ts
+++ src/Interfaces/User.ts
@@ -4,7 +4,7 @@ export interface IUser
     tag: string;
     avatarUrl: string;
     badges: Array<any>;
-    background: string|null;
+    background: string|null|undefined;
     background_url: Boolean;
     createdAt: string;
     circleStrokeColor?: string;
